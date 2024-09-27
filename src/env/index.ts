aa25ad4a2823ed1b38ee829eae04b542d17c869c
Nicoletti <nicoletti@cpqd.com.br>
2024-09-27 18:47:56 -0300

    variávesi de ambiente

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+//process.env: {NODE_ENV: 'dev', ...}
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('production'),
+    PORT: z.coerce.number().default(3333), // coerce converte o valor que for para um numero
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success == false) {
+    console.error('Invalid environment variables.', _env.error.format());
+    throw new Error('Invalid environment variables.');
+}
+
+export const env = _env.data
