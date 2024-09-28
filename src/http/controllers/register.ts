454c13226f27a8eccc02443dcd1e9542f136ded3
Nicoletti <nicoletti@cpqd.com.br>
2024-09-27 23:46:16 -0300

    Hash da senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs"
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
@@ -12,11 +13,23 @@ export const register = async (request: FastifyRequest, response: FastifyReply) 
 
     const { name, email, password } = registryBodySchema.parse(request.body);
 
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (userWithSameEmail) {
+        return response.status(409).send();
+    }
+
     await prisma.user.create({
         data: {
             nome: name,
             email,
-            password_hash: password
+            password_hash,
         }
     });
 
