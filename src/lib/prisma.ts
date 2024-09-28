8a257d0eeb8996526206d84aa4e6482cbd20569b
Nicoletti <nicoletti@cpqd.com.br>
2024-09-27 22:26:25 -0300

    log prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,6 @@
 import { env } from "@/env";
 import { PrismaClient } from "@prisma/client";
 
-
 export const prisma = new PrismaClient({
     //logando queries em ambiente dev
     log: env.NODE_ENV === 'dev' ? ['query'] : []
